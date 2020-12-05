921c69dc43cad31dc86589bf313d2a291b3c5a82
cwkrundong27414 <156264@apps.everettsd.org>
2020-12-05 20:14:47 +0000

    changed speed with console

--- script.js
+++ script.js
@@ -7,4 +7,24 @@ function gameLoop() {
   ctx.fillStyle = "white";
   ctx.rect(0, 0, c.width, c.height);
   ctx.fill();
+  var raindrop = {
+    x: Math.random() * c.width,
+    y:0
+  }
+  raindrops.push(raindrop);
+  var k = raindrops.length;
+  while (k--) {
+    var drop = raindrops[k];
+    drop.y = drop.y + 3;
+    if (drop.y > c.height) {
+      raindrops.splice(k,1);
+    } else {
+    ctx.beginPath()
+    ctx.rect (drop.x,drop.y,2,4 );
+    ctx.fillStyle = "blue";
+    ctx.fill();
+    }
+  }
 }
+
+setInterval(gameLoop, 50);
